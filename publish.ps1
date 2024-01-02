d769851bb6dbfd19fd324380fb69809bb46ee977
tschlottman <tfs423@gmail.com>
2024-01-01 20:10:16 -0600

    fixed sizing issuge in body

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
-$source = 'C:\Workspace\ts-webapp\ts-webapp\docs\browser'
+$source = 'C:\Workspace\ts-webapp\docs\browser'
 
 Get-ChildItem $source -file | move-item -Destination {$_.Directory.Parent.Fullname}
